fcc584db25419e39767b6c8fe34bed89b8dcfa8f
natalie-nishihama <orange18165@gmail.com>
2026-02-23 15:30:05 +0900

    feat 投稿編集機能実装

--- client/react_front/src/App.jsx
+++ client/react_front/src/App.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 
 function App() {
+  //投稿一覧取得用
   const [posts, setPosts] = useState([]);
+  //新規投稿用
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
+  //投稿編集用
+  const [editingId, setEditingId] = useState(null);
 
   // 投稿一覧取得
   const ReadPosts = async () => {
@@ -17,33 +21,72 @@ function App() {
    ReadPosts();
   }, []);
 
-  // 投稿送信
-  const handleSubmit = async (e) => {
-    //そのイベントがもともと行われるべきデフォルトの挙動を防止する
-    e.preventDefault();
 
+// 投稿送信
+const handleSubmit = async (e) => {
+  //そのイベントがもともと行われるべきデフォルトの挙動を防止する
+  e.preventDefault();
+
+  if (editingId) {
+    // 投稿更新
+    await fetch(`/posts/${editingId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content }),
+    });
+  } else {
+    // 投稿新規作成
     await fetch("/posts", {
       method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        title,
-        content,
-        user_id: 1, // 仮で1
-      }),
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ title, content, user_id: 1 }),
     });
+  }
 
-    setTitle("");
-    setContent("");
-   ReadPosts();
-  };
+  setTitle("");
+  setContent("");
+  setEditingId(null);
+  ReadPosts();
+};
+
+
+
+
+  // const handleSubmit = async (e) => {
+    
+  //   e.preventDefault();
+
+  //   await fetch("/posts", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({
+  //       title,
+  //       content,
+  //       user_id: 1, // 仮で1
+  //     }),
+  //   });
+
+  //   setTitle("");
+  //   setContent("");
+  //  ReadPosts();
+  // };
+
+  //投稿編集
+  const startEdit = (post) => {
+  setTitle(post.title);
+  setContent(post.content);
+  setEditingId(post.id);
+};
 
   //投稿削除
   const deletePost = async (id) => {
   await fetch(`/posts/${id}`, {
     method: "DELETE"
   });
+
+
 
  ReadPosts();
 };
@@ -76,6 +119,7 @@ function App() {
         <div key={post.id}>
           <h3>id:{post.id} title:{post.title}</h3>
           <p>{post.content}</p>
+          <button onClick={() => startEdit(post)}>編集</button>
           <button onClick={() => deletePost(post.id)}>削除</button>
         </div>
       ))}
