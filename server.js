210c1d03a65d0dbf9ca95482df37c2ecb85710fe
natalie-nishihama <orange18165@gmail.com>
2026-02-24 09:42:44 +0900

    fix server.js 修正４

--- server.js
+++ server.js
@@ -6,18 +6,32 @@ const postsRoutes = require("./routes/posts");
 const usersRoutes = require("./routes/users");
 const tagsRoutes = require("./routes/tags");
 
-const express = require("express");
 const path = require("path");
-
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//投稿
+app.use("/posts", postsRoutes);
+
+//タグ
+app.use("/tags", tagsRoutes);
+
+// ユーザー
+app.use("/users", usersRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("Server is running");
+// });
+
+// React のビルドフォルダを静的配信
+// app.use("/", express.static(__dirname + "/client/react_front/dist"));
+// app.use(express.static(path.join(__dirname, "/client/react_front/dist")));
 
 // 静的ファイル配信
 const buildPath = path.join(__dirname, "client/react_front/dist");
 app.use(express.static(buildPath));
-
-app.use("/posts", postsRoutes);
-app.use("/users", usersRoutes);
-app.use("/tags", tagsRoutes);
 
 // 静的ファイルに存在しない場合は index.html を返す
 app.use((req, res, next) => {
@@ -30,29 +44,3 @@ app.use((req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// const path = require("path");
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json());
-
-// //投稿
-// app.use("/posts", postsRoutes);
-
-// //タグ
-// app.use("/tags", tagsRoutes);
-
-// // ユーザー
-// app.use("/users", usersRoutes);
-
-// // app.get("/", (req, res) => {
-// //   res.send("Server is running");
-// // });
-
-// // React のビルドフォルダを静的配信
-// app.use("/", express.static(__dirname + "/client/react_front/dist"));
-// app.use(express.static(path.join(__dirname, "/client/react_front/dist")));
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
