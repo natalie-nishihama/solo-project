ed1028a4e7681128bb06e0e063381f0c9a72bc0a
natalie-nishihama <orange18165@gmail.com>
2026-02-24 10:51:23 +0900

    fix デプロイ完了

--- server.js
+++ server.js
@@ -9,42 +9,6 @@ const tagsRoutes = require("./routes/tags");
 const path = require("path");
 const app = express();
 
-// app.use(cors());
-// app.use(express.json());
-
-// //投稿
-// app.use("/posts", postsRoutes);
-
-// //タグ
-// app.use("/tags", tagsRoutes);
-
-// // ユーザー
-// app.use("/users", usersRoutes);
-
-// // app.get("/", (req, res) => {
-// //   res.send("Server is running");
-// // });
-
-// // React のビルドフォルダを静的配信
-// // app.use("/", express.static(__dirname + "/client/react_front/dist"));
-// // app.use(express.static(path.join(__dirname, "/client/react_front/dist")));
-
-// // 静的ファイル配信
-// const buildPath = path.join(__dirname, "client/react_front/dist");
-// app.use(express.static(buildPath));
-
-// // 静的ファイルに存在しない場合は index.html を返す
-// app.use((req, res, next) => {
-//   res.sendFile(path.join(buildPath, "index.html"), (err) => {
-//     if (err) {
-//       next(err);
-//     }
-//   });
-// });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
 // ミドルウェア
 app.use(cors());
 app.use(express.json());
@@ -57,10 +21,9 @@ app.use("/tags", tagsRoutes);
 // React のビルドフォルダを静的配信
 const buildPath = path.join(__dirname, "client", "react_front", "dist");
 console.log("Build path:", buildPath);
-// const buildPath = path.join(__dirname, "client", "react_front", "dist");
 app.use(express.static(buildPath));
 
-// 静的ファイルに存在しない場合は React の index.html にフォールバック
+// 静的ファイルに存在しない場合は React の index.html に投げる
 app.use((req, res, next) => {
   res.sendFile(path.join(buildPath, "index.html"), (err) => {
     if (err) {
